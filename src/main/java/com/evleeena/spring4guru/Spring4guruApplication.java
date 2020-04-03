cc2327d633e455ebf4c8f27bba32b08bff83b1b9
Elena Zaets <evleeena@gmail.com>
2020-04-03 16:11:31 +0300

    initial commit

---
+++ src/main/java/com/evleeena/spring4guru/Spring4guruApplication.java
@@ -0,0 +1,13 @@
+package com.evleeena.spring4guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4guruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4guruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evleeena/spring4guru/Spring4guruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evleeena.spring4guru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring4guruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
